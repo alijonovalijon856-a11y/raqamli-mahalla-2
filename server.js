79630ee6cbf4039e452219b1901df7ae93a31618
Alijon <alijonovalijon856@gmail.com>
2026-05-19 11:22:28 +0500

    restore frontend serving

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const express = require('express');
 
+const path = require('path');
+
 const app = express();
+
+app.use(
+express.static(
+path.join(__dirname,'public')
+)
+);
 
 app.get('/',(req,res)=>{
 
-res.send('SERVER ISHLADI');
+res.sendFile(
+path.join(
+__dirname,
+'public',
+'index.html'
+)
+);
 
 });
 
-app.listen(3000,()=>{
+const PORT =
+process.env.PORT || 3000;
 
-console.log('RUN');
+app.listen(PORT,()=>{
+
+console.log(
+'SERVER ISHLADI'
+);
 
 });
