2c7513077895aa13d2f0a636c9055d822027acf2
Alijon <alijonovalijon856@gmail.com>
2026-05-19 11:36:47 +0500

    real file upload backend

--- server.js
+++ server.js
@@ -2,13 +2,55 @@ const express = require('express');
 
 const path = require('path');
 
+const multer = require('multer');
+
 const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended:true }));
 
 app.use(
 express.static(
 path.join(__dirname,'public')
 )
 );
+
+app.use(
+'/uploads',
+express.static(
+path.join(__dirname,'uploads')
+)
+);
+
+const storage =
+multer.diskStorage({
+
+destination:
+function(req,file,cb){
+
+cb(null,'uploads/');
+
+},
+
+filename:
+function(req,file,cb){
+
+cb(
+null,
+Date.now() +
+'-' +
+file.originalname
+);
+
+}
+
+});
+
+const upload =
+multer({
+storage:storage
+});
 
 app.get('/',(req,res)=>{
 
@@ -19,6 +61,33 @@ __dirname,
 'index.html'
 )
 );
+
+});
+
+app.post(
+'/upload',
+upload.single('file'),
+(req,res)=>{
+
+if(!req.file){
+
+return res.json({
+
+message:
+'File tanlanmagan'
+
+});
+
+}
+
+res.json({
+
+message:
+'File muvaffaqiyatli yuklandi',
+
+file:req.file.filename
+
+});
 
 });
 
