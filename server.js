dbb1550897c131edf318c06548265bc911700279
Alijon <alijonovalijon856@gmail.com>
2026-05-19 11:18:18 +0500

    restore server working

--- server.js
+++ server.js
@@ -1,86 +1,15 @@
-const arizaSchema =
-new mongoose.Schema({
+const express = require('express');
 
-ism:String,
+const app = express();
 
-tur:String,
+app.get('/',(req,res)=>{
 
-user:String,
-
-status:String,
-
-tracking:String,
-
-hujjat:String
+res.send('SERVER ISHLADI');
 
 });
 
-const Ariza =
-mongoose.model(
-'Ariza',
-arizaSchema
-);
+app.listen(3000,()=>{
 
-app.post(
-'/api/ariza',
-upload.single('hujjat'),
-async(req,res)=>{
-
-try{
-
-const yangiAriza =
-new Ariza({
-
-ism:req.body.ism,
-
-tur:req.body.tur,
-
-user:req.body.user,
-
-hujjat:
-req.file
-? req.file.filename
-: '',
-
-status:'pending',
-
-tracking:
-'RM-' +
-Date.now()
-
-});
-
-await yangiAriza.save();
-
-bot.sendMessage(
-8458618683,
-`📥 Yangi ariza!
-
-👤 ${req.body.ism}
-
-📌 ${req.body.tur}
-
-🆔 ${yangiAriza.tracking}`
-);
-
-res.json({
-
-message:
-"Ariza yuborildi"
-
-});
-
-}catch(err){
-
-console.log(err);
-
-res.status(500).json({
-
-message:
-"Saqlashda xatolik"
-
-});
-
-}
+console.log('RUN');
 
 });
